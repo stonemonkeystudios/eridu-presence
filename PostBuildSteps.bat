b0e53936a24ac1bd6548f133b403df036dbe2b87
Ryan Nohr <ryan.nohr@gmail.com>
2022-07-10 12:24:09 -0500

    Update to presence build

--- PostBuildSteps.bat
+++ PostBuildSteps.bat
@@ -1,15 +1,1 @@
-echo "Restoring DotNet Tools for code generators"
-dotnet tool restore
-
-echo "Generating MessagePack Code"
-dotnet mpc -i %~dp0HQDotNet.Presence.Model\HQDotNet.Presence.Model.csproj -o %~dp0HQDotNet.Presence.Client\Assets\HQDotNet.Presence\Generated -m
-echo "Generating Magic Onion Code"
-dotnet moc -i %~dp0HQDotNet.Presence.Model\HQDotNet.Presence.Model.csproj -o %~dp0HQDotNet.Presence.Client\Assets\HQDotNet.Presence\Generated -m
-
-echo "Copy Shared Model files to Unity Client"
-copy %~dp0HQDotNet.Presence.Model\*.cs %~dp0HQDotNet.Presence.Client\Assets\HQDotNet.Presence\Model\
-
-if "%~1"=="-x" goto done
-PAUSE
-
-:done
+copy %~dp0eridu-presence-model\bin\Release\netstandard2.1\eridu-presence-model.dll %~dp0release\
