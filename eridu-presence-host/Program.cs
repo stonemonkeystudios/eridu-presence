311f79bfc509df5f1b019f6ed323b5741da21130
Ryan Nohr <ryan.nohr@gmail.com>
2024-04-28 02:21:21 -0500

    Port updates.

--- eridu-presence-host/Program.cs
+++ eridu-presence-host/Program.cs
@@ -20,7 +20,7 @@ namespace HQDotNet.Presence {
                     webBuilder
                         .UseKestrel(options => {
                             // WORKAROUND: Accept HTTP/2 only to allow insecure HTTP/2 connections during development.
-                            options.ListenAnyIP(443, listenOptions => { listenOptions.Protocols = HttpProtocols.Http2; });
+                            options.ListenAnyIP(5003, listenOptions => { listenOptions.Protocols = HttpProtocols.Http2; });
                         })
                         .UseStartup<Startup>();
                 });
